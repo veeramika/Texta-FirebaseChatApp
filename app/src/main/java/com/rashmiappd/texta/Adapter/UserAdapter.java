32c186076664023e9878c462ab4031f833c6c331
veeramika <panditrk2000@gmail.com>
2020-06-25 01:19:47 +0530

    Users List displayed in recyclerView

---
+++ app/src/main/java/com/rashmiappd/texta/Adapter/UserAdapter.java
@@ -0,0 +1,80 @@
+package com.rashmiappd.texta.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.rashmiappd.texta.Model.User;
+import com.rashmiappd.texta.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;  //User is a class
+   // private boolean ischat;
+
+   // String theLastMessage;
+
+    //Constructor
+    public UserAdapter(Context mContext, List<User> mUsers){
+        this.mUsers = mUsers;
+        this.mContext = mContext;
+       // this.ischat = ischat;
+    }
+
+
+    @NonNull
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+
+        final User user = mUsers.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")){
+            holder.profile_image.setImageResource(R.drawable.emptydp);
+        } else {
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+
+    //ViewHolder class
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+        private ImageView img_on;
+        private ImageView img_off;
+        private TextView last_msg;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            img_on = itemView.findViewById(R.id.img_on);
+            img_off = itemView.findViewById(R.id.img_off);
+            last_msg = itemView.findViewById(R.id.last_msg);
+        }
+    }
+}
